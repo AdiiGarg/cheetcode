c5778d4fa480788ebf29e2563ec0415ea909fc96
Adiii Garg <adiiigarg16@gmail.com>
2025-12-23 02:55:14 +0530

    Submissions page Minor changes!

--- frontend/app/my-submissions/page.tsx
+++ frontend/app/my-submissions/page.tsx
@@ -121,7 +121,7 @@ export default function MySubmissionsPage() {
             <select
               value={sort}
               onChange={e => setSort(e.target.value as any)}
-              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-1 text-xs"
+              className="bg-zinc-900 border border-zinc-800 rounded-lg px-2 py-1 text-xs"
             >
               <option value="latest">Latest</option>
               <option value="oldest">Oldest</option>
